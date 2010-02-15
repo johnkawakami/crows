c1ce1e55bd651868f3cf998f1b14737c750cf00e
Steve Klabnik <steve@steveklabnik.com>
2010-02-14 21:26:27 -0500

    setting up stuff for the panels

---
+++ setup.php
@@ -0,0 +1,9 @@
+<form action="ajax/setup.php" method="POST">
+<table>
+<tr><td>Mysql Username:</td><td><input type="text" name="username" /></td></tr>
+<tr><td>Mysql Password:</td><td><input type="password" name="password" /></td></tr>
+<tr><td>Email:</td><td><input type="text" name="email" /></td></tr>
+<input type="hidden" name="success" value="1" />
+<tr><td></td><td><input type="submit" name="submit" value ="Submit" /></td></tr>
+</table>
+</form>
